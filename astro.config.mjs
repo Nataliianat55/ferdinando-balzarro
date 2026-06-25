db00434c979453f6f60e2e1dffb297900a151318
Nataliianat55 <tashajv@gmail.com>
2026-06-25 11:24:14 +0200

    Add initial Astro configuration

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://ferdinandobalzarro.it',
+  output: 'static',
+  build: { assets: 'assets' },
+  trailingSlash: 'always'
+});
